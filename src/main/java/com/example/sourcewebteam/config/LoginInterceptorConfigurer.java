7dcbd502d5f9f35de93f88fc1d4e233273feba8a
Dantence <84451807+Dantence@users.noreply.github.com>
2022-02-14 19:03:50 +0800

    2022/2/14

--- src/main/java/com/example/sourcewebteam/config/LoginInterceptorConfigurer.java
+++ src/main/java/com/example/sourcewebteam/config/LoginInterceptorConfigurer.java
@@ -1,11 +1,29 @@
 package com.example.sourcewebteam.config;
 
+import com.example.sourcewebteam.interceptor.LoginInterceptor;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class LoginInterceptorConfigurer implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        WebMvcConfigurer.super.addInterceptors(registry);
+       HandlerInterceptor interceptor = new LoginInterceptor();
+       //配置拦截白名单
+       List<String> WhiteLists = new ArrayList<>();
+        WhiteLists.add("/bootstrap3/**");
+        WhiteLists.add("/css/**");
+        WhiteLists.add("/images/**");
+        WhiteLists.add("/js/**");
+        WhiteLists.add("/web/register.html");
+        WhiteLists.add("/web/login.html");
+
+        registry.addInterceptor(interceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(WhiteLists);
     }
 }
