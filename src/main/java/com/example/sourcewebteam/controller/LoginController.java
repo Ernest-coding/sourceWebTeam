c2aa6499ec3986253a267a4f2c753cecaddd0640
Dantence <84451807+Dantence@users.noreply.github.com>
2022-02-16 19:45:51 +0800

    2022/2/16 yyw

--- src/main/java/com/example/sourcewebteam/controller/LoginController.java
+++ src/main/java/com/example/sourcewebteam/controller/LoginController.java
@@ -1,50 +1,50 @@
-package com.example.sourcewebteam.controller;
-
-import com.example.sourcewebteam.entity.UserBean;
-import com.example.sourcewebteam.service.UserService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.annotation.Resource;
-
-@Slf4j
-@Controller
-public class LoginController {
-
-    //将Service注入Web层
-    @Resource
-    UserService userService;
-
-    //登录
-    @RequestMapping("/login")
-    public String show() {
-        return "login";
-    }
-
-    @RequestMapping(value = "/loginIn", method = RequestMethod.POST)
-    public String login(String name, String password) {
-        UserBean userBean = userService.LoginIn(name, password);
-        log.info("username:{}", name);
-        log.info("password:{}", password);
-        if (userBean != null) {
-            return "success";
-        } else {
-            return "error";
-        }
-    }
-
-    @RequestMapping("/signup")
-    public String disp() {
-        return "signup";
-    }
-
-    //注册
-    @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public String signUp(String name, String password) {
-        userService.Insert(name, password);
-        return "success";
-    }
-}
-
+//package com.example.sourcewebteam.controller;
+//
+//import com.example.sourcewebteam.entity.UserBean;
+//import com.example.sourcewebteam.service.UserService;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//
+//import javax.annotation.Resource;
+//
+//@Slf4j
+//@Controller
+//public class LoginController {
+//
+//    //将Service注入Web层
+//    @Resource
+//    UserService userService;
+//
+//    //登录
+//    @RequestMapping("/login")
+//    public String show() {
+//        return "login";
+//    }
+//
+//    @RequestMapping(value = "/loginIn", method = RequestMethod.POST)
+//    public String login(String name, String password) {
+//        UserBean userBean = userService.LoginIn(name, password);
+//        log.info("username:{}", name);
+//        log.info("password:{}", password);
+//        if (userBean != null) {
+//            return "success";
+//        } else {
+//            return "error";
+//        }
+//    }
+//
+//    @RequestMapping("/signup")
+//    public String disp() {
+//        return "signup";
+//    }
+//
+//    //注册
+//    @RequestMapping(value = "/register", method = RequestMethod.POST)
+//    public String signUp(String name, String password) {
+//        userService.Insert(name, password);
+//        return "success";
+//    }
+//}
+//
--- src/main/java/com/example/sourcewebteam/service/UserService.java
+++ src/main/java/com/example/sourcewebteam/service/UserService.java
@@ -1,22 +1,22 @@
-package com.example.sourcewebteam.service;
-
-import com.example.sourcewebteam.entity.UserBean;
-import com.example.sourcewebteam.mapper.UserMapper;
-import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
-
-@Service
-public class UserService {
-    // dao层属性注入service层
-    @Resource
-    private UserMapper userMapper;
-
-    public UserBean LoginIn(String username, String password) {
-        return userMapper.getInfo(username, password);
-    }
-
-    public void Insert(String username, String password) {
-        userMapper.saveInfo(username, password);
-    }
-}
+//package com.example.sourcewebteam.service;
+//
+//import com.example.sourcewebteam.entity.UserBean;
+//import com.example.sourcewebteam.mapper.UserMapper;
+//import org.springframework.stereotype.Service;
+//
+//import javax.annotation.Resource;
+//
+//@Service
+//public class UserService {
+//    // dao层属性注入service层
+//    @Resource
+//    private UserMapper userMapper;
+//
+//    public UserBean LoginIn(String username, String password) {
+//        return userMapper.getInfo(username, password);
+//    }
+//
+//    public void Insert(String username, String password) {
+//        userMapper.saveInfo(username, password);
+//    }
+//}
